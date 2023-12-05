d6e3b6f1e154e409b91451854a182a2ef6904d58
uemura <i_uemura@leading-solutions.co.jp>
2023-12-05 09:13:37 +0900

    no message

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const dateObject = new Date(),
-    year       = dateObject.getFullYear(),
-    month      = dateObject.getMonth(),
-    date       = dateObject.getDate();
-
-console.log(year + '年' + (month + 1) + '月' + date + '日');
